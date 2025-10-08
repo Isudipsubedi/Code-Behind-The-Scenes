511ed817f48e1ed777c417d6424240b21c2cb4dc
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-10-08 11:02:35 +0545

    auth Register and hash the password and insert into db & create a token to help identify particular user

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -5,10 +5,41 @@ import db from '../db.js'
 
 const router = express.Router()
 
-router.post('/register', (req, res)=>{})
+//register new user endpoint /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    //save the username and an irreversibly encrypted password
+    //save sudip | adfakdgjea........esasd
 
-router.post('/login', (req, res)=>{
+    //encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
 
+    //save the new user and hashed password to db
+    try {
+        const insertUser = db.prepare(`INSERT INTO users (username, password)
+        VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword )
+
+        // now that we have a  user, I want to add their first todo for them
+        const defaultTodo = `Hello add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
+        VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token 
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {expiresIn: '24h'})
+        res.json({ token })
+    } catch (err){
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
+})
+
+router.post('/login', (req, res) => {
+    //we get their email and we look up to the password associated with that email in the db
+    //but we get it back and see its encrypted
+    //one way encrypt the password the user just entered
 })
 
 export default router
