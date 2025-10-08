eedd1331ad0f49c388d65c487cbf210e109adf53
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-10-08 14:16:11 +0545

    authmiddleware

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -18,7 +18,7 @@ router.post('/register', (req, res) => {
     try {
         const insertUser = db.prepare(`INSERT INTO users (username, password)
         VALUES (?, ?)`)
-        const result = insertUser.run(username, hashedPassword )
+        const result = insertUser.run(username, hashedPassword)
 
         // now that we have a  user, I want to add their first todo for them
         const defaultTodo = `Hello add your first todo!`
@@ -27,9 +27,9 @@ router.post('/register', (req, res) => {
         insertTodo.run(result.lastInsertRowid, defaultTodo)
 
         // create a token 
-        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {expiresIn: '24h'})
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
         res.json({ token })
-    } catch (err){
+    } catch (err) {
         console.log(err.message)
         res.sendStatus(503)
     }
@@ -41,29 +41,29 @@ router.post('/login', (req, res) => {
     //but we get it back and see its encrypted
     //one way encrypt the password the user just entered
 
-    const {username, password} = req.body
-    
+    const { username, password } = req.body
+
     try {
-        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?',[username])
         const user = getUser.get(username)
 
         // if we cannot find a user associated with that username, return out from the function
-        if (!user) {return res.status(404).send({message:"User not found" })}
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
 
         const passwordIsValid = bcrypt.compareSync(password, user.password)
         //if password does not match, return out of the function
-        if (!passwordIsValid) {return res.status(401).send({message:"invalid password"})}
+        if (!passwordIsValid) { return res.status(401).send({ message: "invalid password" }) }
 
         console.log(user)
 
 
         // then we have a succcessful authentication
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
         res.json({ token })
     } catch (err) {
         console.log(err.message)
         res.sendStatus(503)
-        
+
     }
 })
 
